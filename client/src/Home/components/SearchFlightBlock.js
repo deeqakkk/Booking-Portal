95115ac0049905c19fea993a33e5bed5695016d3
Kalpesh Khairnar <khairnar.kalpesh15@gmail.com>
2021-11-20 13:36:03 +0530

    Task #3 changed month in data selection in search component

--- client/src/Home/components/SearchFlightBlock.js
+++ client/src/Home/components/SearchFlightBlock.js
@@ -235,18 +235,18 @@ const SearchFlightBlock = () => {
     var dd = weekday[d.getDay()];
 
     const month = [12];
-      month[0] = "Jan";
-      month[1] = "Feb";
-      month[2] = "Mar";
-      month[3] = "Apr";
-      month[4] = "May";
-      month[5] = "Jun";
-      month[6] = "Jul";
-      month[7] = "Aug";
-      month[8] = "Sep";
-      month[9] = "Oct";
-      month[10] = "Nov";
-      month[11] = "Dec";
+    month[0] = "Jan";
+    month[1] = "Feb";
+    month[2] = "Mar";
+    month[3] = "Apr";
+    month[4] = "May";
+    month[5] = "Jun";
+    month[6] = "Jul";
+    month[7] = "Aug";
+    month[8] = "Sep";
+    month[9] = "Oct";
+    month[10] = "Nov";
+    month[11] = "Dec";
     var mm = month[d.getMonth()];
 
     dispatch(
