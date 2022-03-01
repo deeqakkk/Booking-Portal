4dc9a09d916371dbefed93cb8b024c8013877fdb
Deepak <0822deepakverma@gmail.com>
2022-03-01 16:08:26 +0530

    Revert "Merge pull request #21 from krishna120801/main"
    This reverts commit 9d0987057f4ccda435f02aea617babf6346f7503, reversing
    changes made to cb83ac9b32340c7cb4543dffbabfa87eb2a3501b.

--- client/src/Home/HomePage.js
+++ client/src/Home/HomePage.js
@@ -1,56 +1,12 @@
 import React from "react";
-import { useEffect, useState } from "react";
 import Header from "../Header/index";
 import SearchFlightBlock from "./components/SearchFlightBlock";
 import MyButton from "../Utilities/Button";
 import { useSelector, useDispatch } from "react-redux";
 import "./homePage.css";
 import Footer from "../Footer/Footer";
-import Faq from "react-faq-component";
 ///REDUX
 import { setError } from "../actions/HomePageActions";
-const data = {
-  rows: [
-      {
-          title: "1)	How far in advance can your flight be booked?",
-          content: `You can easily book your flight two hours prior to the time of departure. Also, you can book your flight 330 days early.`,
-      },
-      {
-          title: "2)	How can I obtain the information about my flight?",
-          content:
-              "You can contact our customer service which is available 24*7.",
-      },
-      {
-          title: "3)	Can I cancel my flight booking?",
-          content: `Yes, you can do that. `,
-      },
-      {
-          title: "4)	Can I book a private jet flight?",
-          content: "Yes, you can do that.",
-      },
-      {
-        title: "5)	How can I save more on my flight bookings?",
-        content: "Look for offers and book accordingly. ",
-    },
-  ],
-};
-
-const styles = {
-  rowContentPaddingTop: '10px',
-        rowContentPaddingBottom: '10px',
-        rowContentPaddingLeft: '50px',
-        rowContentPaddingRight: '150px',
-  titleTextColor: "black",
-  rowTitleColor: "black",
-  // rowContentColor: 'grey',
-  // arrowColor: "red",
-};
-
-const config = {
-   animate: true,
-  arrowIcon: "",
-  tabFocus: true
-};
 const HomePage = () => {
   const dispatch = useDispatch();
   const departure = useSelector((state) => state.Home.departure);
@@ -135,22 +91,6 @@ const HomePage = () => {
           linkTo={handleLinks()}
         />
       </div>
-      <div className="container">
-        <h3 className="text-center fw-bold mt-5 mb-5">
-          What Travelers Are Asking? 
-        </h3>
-        <div className="bg-light">
-        <Faq
-                data={data}
-                styles={styles}
-                config={config}
-            />
-          <div className="row mx-auto justify-content-center my-auto">
-            <input className="queryinp col-6 my-auto"placeholder="Ask your query Here" type="text"/>
-            <button className="askbtn col-6"type="submit">Ask now</button>
-          </div>
-        </div>
-        </div>
       <div class="container text-center mt-5">
         <h4>
           <b>About us</b>
