17ca6aaaef79fbe8609af4a52fe3257482cf297f
Deepak <0822deepakverma@gmail.com>
2022-03-05 20:13:53 +0530

    images updated in offer section

--- client/src/Offers/Offers.js
+++ client/src/Offers/Offers.js
@@ -13,7 +13,7 @@ function Offers() {
                         <b>Fly0kart</b>
 
                         <div className='banner-btn mt-5'>
-                            <a href='#'>Book Now</a>
+                            <a href='!#'>Book Now</a>
                         </div>
 
                     </div>
@@ -22,11 +22,11 @@ function Offers() {
             <div className='row my-5'>
                 <div className='col-12 col-md-6 left-container'>
                     <div className='offer-card'>
-                        <img src={singapore} className='img-fluid'/>
+                        <img src="https://cdn.pixabay.com/photo/2021/09/07/11/53/car-6603726_960_720.jpg" className='img-fluid' alt='offer'/>
                         <div className='row mx-1 mx-md-5 my-2'>
                             <div className='col-6'>singapore</div>
                             <div className='col-6'>
-                                <a className='float-end save'>Save 23%</a>
+                                <a className='float-end save' href='!#'>Save 23%</a>
                             </div>
                         </div>
                         <div className='row mx-1 mx-md-5 my-2 pb-4'>
@@ -41,11 +41,11 @@ function Offers() {
                     <div className='row'>
                         <div className='col-12 col-md-6'>
                             <div className='offer-card'>
-                                <img src={singapore} className='img-fluid'/>
+                                <img src="https://cdn.pixabay.com/photo/2016/08/15/08/22/greece-1594689_960_720.jpg" className='img-fluid' alt='offers'/>
                                 <div className='row mx-1 my-1'>
                                     <div className='col-6'>singapore</div>
                                     <div className='col-6 '>
-                                        <a className='float-end save'>Save 23%</a>
+                                        <a className='float-end save' href='!#'>Save 23%</a>
                                     </div>
                                 </div>
                                 <div className='row mx-1 my-1 pb-2'>
@@ -58,11 +58,11 @@ function Offers() {
                         </div>
                         <div className='col-12 col-md-6'>
                             <div className='offer-card'>
-                                <img src={singapore} className='img-fluid'/>
+                                <img src="https://cdn.pixabay.com/photo/2020/06/18/18/48/highway-5314645_960_720.jpg" className='img-fluid' alt='offers'/>
                                 <div className='row mx-1 my-1'>
                                     <div className='col-6'>singapore</div>
                                     <div className='col-6 '>
-                                        <a className='float-end save'>Save 23%</a>
+                                        <a className='float-end save' href='!#'> Save 23%</a>
                                     </div>
                                 </div>
                                 <div className='row mx-1 my-1 pb-2'>
@@ -78,11 +78,11 @@ function Offers() {
                     <div className='row mt-1'>
                         <div className='col-12 col-md-6'>
                             <div className='offer-card'>
-                                <img src={singapore} className='img-fluid'/>
+                                <img src="https://cdn.pixabay.com/photo/2022/02/15/07/27/travel-7014427_960_720.jpg" className='img-fluid' alt='offers'/>
                                 <div className='row mx-1 my-1'>
                                     <div className='col-6'>singapore</div>
                                     <div className='col-6 '>
-                                        <a className='float-end save'>Save 23%</a>
+                                        <a className='float-end save' href='!#'>Save 23%</a>
                                     </div>
                                 </div>
                                 <div className='row mx-1 my-2 pb-2'>
@@ -95,11 +95,11 @@ function Offers() {
                         </div>
                         <div className='col-12 col-md-6'>
                             <div className='offer-card'>
-                                <img src={singapore} className='img-fluid'/>
+                                <img src="https://cdn.pixabay.com/photo/2020/12/17/04/09/guanajuato-5838206_960_720.jpg" className='img-fluid' alt='offers'/>
                                 <div className='row mx-1 my-1'>
                                     <div className='col-6'>singapore</div>
                                     <div className='col-6 '>
-                                        <a className='float-end save'>Save 23%</a>
+                                        <a className='float-end save' href='!#'>Save 23%</a>
                                     </div>
                                 </div>
                                 <div className='row mx-1 my-2 pb-2'>
